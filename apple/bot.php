b3c848595d4cccea35add9408ce7d24131b95551
Yomohero <textplusoladipupo@gmail.com>
2025-01-15 12:32:01 -0800

    Update bot.php

--- apple/bot.php
+++ apple/bot.php
@@ -10,7 +10,7 @@ if($message == "/start") {
 }
 
 function sendMessage($chat_id, $message) {
-    $apiToken = "7930320812:AAH264MiuNQHIF7K18lb7Cf7R2eZlfKK1fw";
+    $apiToken = "8102555748:AAFvecafSlPlMDYpITc-041L_KAnGNWbXb4";
     $url = "https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=".urlencode($message);
     file_get_contents($url);
 }
